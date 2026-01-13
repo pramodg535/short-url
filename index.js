7dd347e3e40d3c7c9ae5180f97ffbc94762161ff
Pramod G <gpramodh535@gmail.com>
2026-01-13 13:08:38 +0530

    Fix frontend serving and routing

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 
 const PORT = process.env.PORT || 8001;
 const app=express();
@@ -20,11 +26,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "frontend")));
 app.use("/url",urlRoute);
 
-app.get('/', (req, res) => {
-  res.send('Short URL API is running!');
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
+
 
 app.get("/:shortId", async (req, res) => {
   const { shortId } = req.params;
